bb2fe28a3ea2adc402f2239f5c7eb3f70f694f2b
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-09 23:22:16 -0300

    style: add animation in Presentation component

--- components/cards/presentation/Presentation.tsx
+++ components/cards/presentation/Presentation.tsx
@@ -1,17 +1,25 @@
+"use client"
 import styles from './styles.module.css'
+import { useEffect, useState } from 'react'
 import { Button } from '@/components'
 export const Presentation = () => {
+  const [active,setActive] = useState<'active' | undefined>()
+  useEffect(() => {
+    setActive('active')
+  },[])
   return (
     <section className={ styles.container }>
       <div className={ styles.content}>
-        <h3>
+        <h3 className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
           O seu futuro começa hoje!
         </h3>
-        <h2>
+        <h2 className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
           Aprenda se divertindo
           e realize seus sonhos!
         </h2>
-        <p>Somos um núcleo tecnológico de aprendizado inovador para crianças, adolescentes e jovens.</p>
+        <p className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
+          Somos um núcleo tecnológico de aprendizado inovador para crianças, adolescentes e jovens.
+        </p>
         <Button.Default styleTypes='btn-presentation' text='Agende sua aula presencial' type='button' />
 
       </div>
