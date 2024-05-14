119bb1e4ca2953ed24b9d9ce0e6647f426e8a521
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-14 03:32:34 -0300

    refactor: change Presentation component style

--- components/cards/presentation/Presentation.tsx
+++ components/cards/presentation/Presentation.tsx
@@ -1,38 +1,33 @@
 "use client"
 import { FaCalendarDays } from "react-icons/fa6";
 import styles from './styles.module.css'
-import { useEffect, useState } from 'react'
+import Image from 'next/image';
 import { Button, Icon } from '@/components'
-import { BackgroundCover } from '@/components';
+import Technology from "../../../public/images/technology.svg"
+
 
 export const Presentation = () => {
-  const [active,setActive] = useState<'active' | undefined>()
-  useEffect(() => {
-    setActive('active')
-  },[])
   return (
     <section className={ styles.container }>
       <article className={ styles.content}>
-        <h3 className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
-          O seu futuro começa hoje!
-        </h3>
-        <h2 className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
+        <h2 className={styles.title}>
           Aprenda se divertindo
           e realize seus sonhos!
         </h2>
-        <p className={ active ? `${styles.title}   ${styles[active]}` : styles.title }>
+        <p className={ styles.title }>
           Somos um núcleo tecnológico de aprendizado inovador para crianças, adolescentes e jovens.
         </p>
-        <Button.Default styleTypes='btn-presentation' text='Agende sua aula presencial' type='button'>
-            <Icon styleType={"default"} icon={FaCalendarDays} size={23}/>
-        </Button.Default>
+          <Button.Default styleTypes='btn-presentation' text='Agende sua aula presencial' type='button'>
+              <Icon styleType={"default"} icon={FaCalendarDays} size={23}/>
+          </Button.Default>
       </article>
-      <span className={styles.cover}></span>
-      <BackgroundCover           
-          alt='minecraft gif'
-          src='/gifs/home.gif'
-          fill
-      />
+      <article className={ styles.icon}>
+        <Technology />
+
+      </article>
+      <span className={ styles.circle }>
+
+      </span>
     </section>
   )
 }
