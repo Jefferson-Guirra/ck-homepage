780d534467f79171c15703dd2725b4aaf1f05b21
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-05-23 23:56:49 -0300

    refactor: remove unnecessary code

--- components/motion-components/opacity/MotionOpacity.tsx
+++ components/motion-components/opacity/MotionOpacity.tsx
@@ -1,19 +1,22 @@
 "use client"
 import { motion } from "framer-motion"
-import { ElementType, ReactNode } from "react"
+import { ReactNode } from "react"
 
 interface Props {
   children: ReactNode
   margin?: string 
+  delay?: number
 }
 
 export const MotionOpacity = ({...props}: Props) => {
-  const {children, margin} = props
+  const {children, margin, delay} = props
   return (
     <motion.div
       initial={{ opacity: 0}}
       whileInView={{ opacity: 1 }}
-      viewport={{ margin }}>
+      viewport={{ margin }}
+      transition={{ delay }}
+      >
         {children}
     </motion.div>
   )
